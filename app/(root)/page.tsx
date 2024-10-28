ea7c1145c0110119ab7df6778ed5fe9b84ebc7a2
clnt <c.connery14@gmail.com>
2024-10-28 01:03:01 +0100

    startupcard creation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,6 +7,21 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "John Doe" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://dims.apnews.com/dims4/default/c1c65f3/2147483647/strip/true/crop/2500x1667+0+0/resize/1440x960!/format/webp/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F37%2F96%2Fba076bf078a5405ce47a04a08321%2Fe85d3581bf024703a51bb2dad6ab43c0",
+      category: "Tech",
+      title: "Startup 1",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -18,6 +34,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post, index) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p>No results</p>
+          )}
+        </ul>
       </section>
     </>
   );
